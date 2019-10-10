04b927a72ade5d4a91066b081d61695c5c4acad0
Bhupal <poubhupal@gmail.com>
2019-10-10 18:39:39 -0400

    Second commit

--- Sel_Jen_Git_Project/src/test/java/mavenJenkinsBuildTest/MavenTestRunToJenkin.java
+++ Sel_Jen_Git_Project/src/test/java/mavenJenkinsBuildTest/MavenTestRunToJenkin.java
@@ -14,8 +14,10 @@ public class MavenTestRunToJenkin {
 		int a = 10;
 		int b = 30;
 		int s = a+b;
+		int d = b-a;
 		
 		System.out.println("Sum of a and b is: "+s);
+		System.out.println("Difference of b and a is: "+d);
 		
 	}
 
